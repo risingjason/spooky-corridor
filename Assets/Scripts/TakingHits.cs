b4cde1d5b4e70c8da804b6145f2cd7645ad1503c
Jesse Huang <39164387+jjhuang417@users.noreply.github.com>
2025-10-26 02:40:41 -0700

    Added HP implementation taking hits. (#18)

--- Assets/Scripts/TakingHits.cs
+++ Assets/Scripts/TakingHits.cs
@@ -3,19 +3,28 @@ using UnityEngine;
 public class TakingHits : MonoBehaviour
 {
     [SerializeField] int fearHitPoints = 3;
+    [SerializeField] RectTransform fearMeterForeGround; // assign in Inspector
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Trap")
         {
-            fearHitPoints--;
-            handleEnemyDeath();
+            // Read current width
+            float currentWidth = fearMeterForeGround.sizeDelta.x;
+            Debug.Log("Current width: " + currentWidth);
+
+            // Increment width (example: increase by 10)
+            Vector2 size = fearMeterForeGround.sizeDelta;
+            size.x += 100/3f;
+            fearMeterForeGround.sizeDelta = size;
+            Debug.Log("new numbers for: " + size);
+            handleEnemyDeath(size);
         }
     }
 
-    private void handleEnemyDeath()
+    private void handleEnemyDeath(Vector2 size)
     {
-        if (fearHitPoints <= 0)
+        if (size.x >= 100)
         {
             Destroy(this.gameObject);
         }
