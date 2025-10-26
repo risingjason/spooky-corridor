0fb82c82326f7f2630d0bb3aea6d76db27a275b2
Jason Zhang <jzhang5137@gmail.com>
2025-10-26 13:23:27 -0700

    feat(main-menu): adds main menu scene with transition to level scene

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    private UIDocument document;
+    private Button button;
 
+    private void Awake()
+    {
+        document = GetComponent<UIDocument>();
+        button = document.rootVisualElement.Q<Button>("StartGameButton") as Button;
+        button.RegisterCallback<ClickEvent>(OnStartGameClick);
+    }
+
+    private void OnDisable()
+    {
+        button.UnregisterCallback<ClickEvent>(OnStartGameClick);
+    }
+
+    private void OnStartGameClick(ClickEvent evt)
+    {
+        LoadSceneByName("level");
+    }
+
+    private void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
